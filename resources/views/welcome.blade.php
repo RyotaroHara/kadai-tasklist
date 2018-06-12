352eabe19e5029ec306d604c30d54483ea67c8bc
ryotaro.hara <ryotaro.hara@rakuten.com>
2018-06-12 06:34:56 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+  
+  
+  @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        {{ $user->name }}
+    @else
+  
+  
+  
+  
+  
+  
+  
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the Tasklists</h1>
@@ -8,4 +21,6 @@
 
         </div>
     </div>
+    
+    @endif
 @endsection
