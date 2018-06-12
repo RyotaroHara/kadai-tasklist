681597506feea1d2e2b4cb60e41a7389ce2791ee
ryotaro.hara <ryotaro.hara@rakuten.com>
2018-06-12 06:22:31 +0000

    user registration

--- resources/views/commons/error_tasks.blade.php
+++ resources/views/commons/error_tasks.blade.php
@@ -1,6 +1,7 @@
 @if (count($errors) > 0)
         <ul>
             @foreach ($errors->all() as $error)
+              <div class="alert alert-warning">{{ $error }}</div>
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
